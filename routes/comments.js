b5a7c6875449fe0535d4f0ed3041e416fc9cdad3
Alex Petit <alexpetit6@gmail.com>
2023-05-29 20:28:56 -0700

    protected post privileges for posts and comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var commentsCtrl = require('../controllers/comments.js')
+var commentsCtrl = require('../controllers/comments')
+var ensureLoggedIn = require('../config/ensureLoggedIn')
 
 router.post('/posts/:id/comments', commentsCtrl.create)
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var postsCtrl = require('../controllers/posts')
+var ensureLoggedIn = require('../config/ensureLoggedIn')
 
 router.get('/', postsCtrl.index)
-router.get('/new', postsCtrl.new)
+router.get('/new', ensureLoggedIn, postsCtrl.new)
 router.get('/:id', postsCtrl.show)
-router.post('/', postsCtrl.create)
+router.post('/', ensureLoggedIn, postsCtrl.create)
 
 module.exports = router;
