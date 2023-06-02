6402e1a3227d66539e0080528f5b78b0743a4323
Alex Petit <alexpetit6@gmail.com>
2023-06-01 22:49:35 -0700

    added more

--- controllers/comments.js
+++ controllers/comments.js
@@ -7,9 +7,9 @@ module.exports = {
 
 async function create(req, res) {
     const post = await Post.findById(req.params.id);
-    req.body.user = req.user._id
-    req.body.userName = req.user.name
-    post.comments.push(req.body)
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    post.comments.push(req.body);
     try {
         await post.save()
         res.redirect(`/posts/${post._id}`)
