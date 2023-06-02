c2f7dd7afe8a656ab7ab493ac1b5668e06b68679
Alex Petit <alexpetit6@gmail.com>
2023-06-01 22:49:00 -0700

    added a few semi-colons

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var postsCtrl = require('../controllers/posts')
 var ensureLoggedIn = require('../config/ensureLoggedIn')
 
-router.get('/', postsCtrl.index)
-router.get('/new', ensureLoggedIn, postsCtrl.new)
-router.get('/:id', postsCtrl.show)
-router.get('/:id/edit', ensureLoggedIn, postsCtrl.edit)
-router.post('/', ensureLoggedIn, postsCtrl.create)
-router.delete('/:id', ensureLoggedIn, postsCtrl.delete)
-router.put('/:id', ensureLoggedIn, postsCtrl.update)
+router.get('/', postsCtrl.index);
+router.get('/new', ensureLoggedIn, postsCtrl.new);
+router.get('/:id', postsCtrl.show);
+router.get('/:id/edit', ensureLoggedIn, postsCtrl.edit);
+router.post('/', ensureLoggedIn, postsCtrl.create);
+router.delete('/:id', ensureLoggedIn, postsCtrl.delete);
+router.put('/:id', ensureLoggedIn, postsCtrl.update);
 
 module.exports = router;
